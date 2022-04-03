76beab75b41f4a6d4012c3961c652f2fd432eff2
Mithun H B <msdmithun07@gmail.com>
2022-04-03 06:57:15 +0530

    productgallary

--- fatalkart/views.py
+++ fatalkart/views.py
@@ -5,9 +5,15 @@ from store.models import *
 def home(request):
     products = Product.objects.all().filter(is_available=True).order_by('created_date')
     
+    # Get the reviews
+    reviews = None
+    for product in products:
+        reviews = ReviewRating.objects.filter(product_id=product.id, status=True)
+
     context = {
         'products': products,
- 
+        'reviews': reviews,
     }
+    
     return render(request, "kart/home.html", context)
-       # 'reviews': reviews,
+    
